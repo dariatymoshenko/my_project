5037b5f17de2d669afa9680f0903ac8882ed67b8
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2021-02-18 11:45:00 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('HOST', 'localhost');    // адрес сервера 
+define('DATABASE', 'website_bd');   // имя базы данных
+define('USER', 'root'); 	// имя пользователя
+define('PASSWORD', 'root');		// пароль
