b9e4f0fb980b08ef420814382c9acc869ad99b39
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2021-02-18 11:44:08 +0200

    Add files via upload

--- OneNews.php
+++ OneNews.php
@@ -5,14 +5,20 @@ namespace App\Controllers\Frontend;
 use App\Models\News as NewsModel;
 use App\Services\ViewLoader;
 
-class News
+class OneNews
 {
+    private $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
     public function render()
     {
         $model = new NewsModel();
         $view = new ViewLoader();
-        $view->loadTemplate('news.php', [
-            'data' => $model->getNews(),
+        $view->loadTemplate('one_news.php', [
+            'row' => $model->getOneNews($this->id),
         ]);
     }
 }
