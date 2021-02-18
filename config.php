177fcbca254bb26edcc41457acf63952631aad78
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2021-02-18 11:42:08 +0200

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('HOST', 'localhost');    // адрес сервера 
-define('DATABASE', 'obj4');   // имя базы данных
-define('USER', 'root'); 	// имя пользователя
-define('PASSWORD', 'root');		// пароль
